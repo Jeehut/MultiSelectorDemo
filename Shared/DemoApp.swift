b4f98f499b3946c47754484dde16b1f1a5723229
Cihat Gündüz <commits@cihatguenduez.de>
2020-07-25 22:14:30 +0200

    Initial Commit

---
+++ Shared/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Shared
+//
+//  Created by Cihat on 25.07.20.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
